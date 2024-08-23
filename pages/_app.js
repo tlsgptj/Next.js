51e0c059e5a45cfada934f0d27d6d96aec03a13f
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-08-23 13:37:31 +0900

    app.js에 공통된 컴포넌트 넣기

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header/>
+      <Container>
+        <Component {...pageProps}/>
+      </Container>
+    </>
+  );
 }
