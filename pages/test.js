35a51cfd7f6f637134036bb30c4606fee742e166
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-08-23 14:07:47 +0900

    image 컴포넌트 소개

---
+++ pages/test.js
@@ -0,0 +1,13 @@
+export default function Test() {
+    return (
+      <>
+        <img
+          src="/images/product.jpeg"
+          width="400"
+          height="400"
+          alt="상품 이미지"
+        />
+      </>
+    );
+  }
+  
