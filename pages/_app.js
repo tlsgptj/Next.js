6a5e8c970053eef34a4c479fc92160a710146db5
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-08-23 13:45:39 +0900

    다크 모드 만들기

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
+import { ThemeProvider } from "@/lib/ThemeContext";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider>
       <Header/>
       <Container>
         <Component {...pageProps}/>
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
