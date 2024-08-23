8ffb22b9316d04a26f18a0057d627061f257d349
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-08-23 13:19:46 +0900

    search.js 수정

--- pages/search.js
+++ pages/search.js
@@ -1,10 +1,22 @@
 import ProductList from "@/components/ProductList";
 import SearchForm from "@/components/SearchForm";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Search() {
+    const [products, setProducts] = useState([]);
     const router = useRouter();
     const { q } = router.query;
+
+    async function getProducts(query) {
+        const res = await axios.get(`/products/?q=${query}`);
+        const nextProducts = res.data.results;
+        setProducts(nextProducts);
+    }
+
+    useEffect(() => {
+        getProducts(q);
+    }, [q]);
 
     return (
         <div>
