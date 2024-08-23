6a8d2acb6ae23f7b7e119baea93b3aa7d30661e9
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-08-23 13:29:25 +0900

    리다이렉트

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  //리다이렉트 : 경로 변경 source : 현재주소, destination: 변경 주소
+  redirects: async function redirects() {
+    return [
+      {
+        source: '/products/:id',
+        destination: '/items/:id',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
